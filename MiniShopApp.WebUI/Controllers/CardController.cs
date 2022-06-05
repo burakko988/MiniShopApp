2bc5c1d832821db23daef5c9961a019df19f7347
QijiBK <77571435+burakko988@users.noreply.github.com>
2022-06-06 00:05:14 +0300

    Initial commit

---
+++ MiniShopApp.WebUI/Controllers/CardController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using MiniShopApp.Business.Abstract;
+using MiniShopApp.WebUI.Identity;
+using MiniShopApp.WebUI.Models;
+using System.Linq;
+
+namespace MiniShopApp.WebUI.Controllers
+{
+    public class CardController : Controller
+    {
+       
+            private ICardService _cardService;
+        private UserManager<User> _userManager;
+        public CardController(ICardService cardService, UserManager<User> userManager)
+        {
+            _cardService = cardService;
+            _userManager = userManager;
+        }
+
+        public IActionResult Index()
+        {
+            var userId = _userManager.GetUserId(User);
+            var card = _cardService.GetCardByUserId(userId);
+            return View(new CardModel()
+            {
+                CardId = card.Id,
+                CardItems= card.CardItems.Select(i=>new CardItemModel()
+                {
+                    CardItemId = i.Id,
+                    ProductId = i.ProductId,
+                    Name = i.Product.Name,
+                    Price =(decimal)i.Product.Price,
+                    ImageUrl = i.Product.ImageUrl,
+                    Quantity = i.Quantity
+                }).ToList()
+            });
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# MiniShopApp
+ 
